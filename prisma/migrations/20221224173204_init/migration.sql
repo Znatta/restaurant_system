5ad5b0389d952774cbe2e0176854e6bd874eff8b
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2022-12-24 14:35:30 -0300

    ✨ feat: Prisma init

---
+++ prisma/migrations/20221224173204_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Table` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `occupied` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
